1e8af3dfad5460849f25708a41541f0a0b6913b5
Rickard Laurin <rickard.laurin@gmail.com>
2014-04-07 13:49:36 +0200

    google maps heat layer, full screen map

--- partial/map/map.js
+++ partial/map/map.js
@@ -2,11 +2,27 @@ angular.module('lab-heatmap').controller('MapCtrl', function ($scope) {
   
   'use strict';
 
+  var MockHeatLayer = function (heatLayer) {
+    var map, pointArray, heatmap;
+
+    var data = [
+      new google.maps.LatLng(59.2124659, 18.0216075),
+    ];
+
+    pointArray = new google.maps.MVCArray(data);
+    heatLayer.setData(pointArray);
+  };
+
   $scope.map = {
     center: {
-      latitude: 45,
-      longitude: -73
+      latitude: 59.2124659,
+      longitude: 18.0216075
     },
+    heatLayerCallback: function (layer) {
+      //set the heat layers backend data
+      var mockHeatLayer = new MockHeatLayer(layer);
+    },
+    showHeat: true,
     zoom: 8
   };
 
