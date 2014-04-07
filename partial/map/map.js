b5a27fdfca7bdbbfbc21d4b8ad99bda9e1f959b3
Rickard Laurin <rickard.laurin@gmail.com>
2014-04-07 13:13:34 +0200

    map and stuffz

--- partial/map/map.js
+++ partial/map/map.js
@@ -1,4 +1,13 @@
 angular.module('lab-heatmap').controller('MapCtrl', function ($scope) {
+  
   'use strict';
+
+  $scope.map = {
+    center: {
+      latitude: 45,
+      longitude: -73
+    },
+    zoom: 8
+  };
 
 });
